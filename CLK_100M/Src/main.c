0c28131c7d0d3256df86b457e9475ace771b01c4
vboxuser <vboxuser@10.0.2.15>
2026-07-28 16:20:47 -0400

    Systick exception added

--- CLK_100M/Src/main.c
+++ CLK_100M/Src/main.c
@@ -1,37 +1,26 @@
 #include "led.h"
 #include "clk.h"
+#include "systick.h"
 
-//SysTick	CortexM4GenericUserGuide
-#define	COUNTFLAG		(1U<<16)
-#define CLKSOURCE		(1U<<2)
-#define	ENABLE			(1U<<0)
-
-void systick_init(void)
-{
-	SysTick->LOAD	=	100000000 /10 - 1;
-	SysTick->VAL	=	0;
-	SysTick->CTRL	|=	(CLKSOURCE | ENABLE);
-}
+volatile uint16_t	mill_sec = 0;
 
 int main(void)
 {
 	led_init();
 	clk_100m_init();
-
-	systick_init();
-
-	uint8_t tencount = 0;
+	systick_1ms_timer();
 
 	while(1)
 	{
-		if(SysTick->CTRL & COUNTFLAG)
-		{
-			tencount++;
-			if(tencount >= 10)
-			{
-				GPIOA->ODR	^=	ODR_PA5_H;
-				tencount = 0;
-			}
-		}
+
+	}
+}
+void SysTick_Handler(void)
+{
+	mill_sec ++;
+	if(mill_sec >= 1000)
+	{
+		mill_sec = 0;
+		GPIOA->ODR	^=	ODR_PA5_H;
 	}
 }
---
+++ CLK_100M/Src/systick.c
@@ -0,0 +1,15 @@
+#include "systick.h"
+
+//SysTick	CortexM4GenericUserGuide
+//#define	COUNTFLAG		(1U<<16)
+#define CLKSOURCE		(1U<<2)
+#define TICKINT			(1U<<1)
+#define	ENABLE			(1U<<0)
+
+void systick_1ms_timer(void)
+{
+	SysTick->LOAD	=	(100000000 * 0.001) - 1;
+	SysTick->VAL	=	0;
+	SysTick->CTRL	|=	(CLKSOURCE | TICKINT | ENABLE);
+	NVIC_EnableIRQ(SysTick_IRQn);
+}
