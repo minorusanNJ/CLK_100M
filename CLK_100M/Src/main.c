844777559af997284838d6b264411ad8a1ced2ac
vboxuser <vboxuser@10.0.2.15>
2026-07-30 13:44:32 -0400

    Uart added

--- CLK_100M/Src/main.c
+++ CLK_100M/Src/main.c
@@ -1,18 +1,26 @@
 #include "led.h"
 #include "clk.h"
 #include "systick.h"
+#include "uart.h"
+#include <stdio.h>
 
 volatile uint16_t	mill_sec = 0;
+volatile uint8_t	timer1 = 0;
 
 int main(void)
 {
 	led_init();
 	clk_100m_init();
 	systick_1ms_timer();
+	uart_init();
 
 	while(1)
 	{
-
+		if(timer1)
+		{
+			printf("UART is working... \n\r");
+			timer1 = 0;
+		}
 	}
 }
 void SysTick_Handler(void)
@@ -22,5 +30,6 @@ void SysTick_Handler(void)
 	{
 		mill_sec = 0;
 		GPIOA->ODR	^=	ODR_PA5_H;
+		timer1 = 1;
 	}
 }
